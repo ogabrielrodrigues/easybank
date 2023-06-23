a7ef2fd6b729a754e0dc3921dfec91b298d61bc6
ogabrielrodrigues <gabrielrodrigues.dev409@gmail.com>
2023-06-23 14:44:08 -0300

    mobile, tablet, notebook

---
+++ index.js
@@ -0,0 +1,27 @@
+document.querySelector('#btn-menu').addEventListener('click', toggleMenu)
+
+function toggleMenu() {
+    const wrapper = document.querySelector('#wrapper')
+    const menu_mobile = document.querySelector('#menu-mobile')
+    const btn_menu = document.querySelector('#menu-open')
+    const btn_close = document.querySelector('#menu-close')
+
+    menu_mobile.classList.toggle('hidden')
+    wrapper.classList.toggle('hidden')
+    btn_menu.classList.toggle('hidden')
+    btn_close.classList.toggle('hidden')
+}
+
+[...document.querySelectorAll('#menu-desktop > div')].forEach(item => {
+    item.addEventListener('click', setActualMenu)
+})
+
+function setActualMenu(e) {
+    [...document.querySelectorAll('#menu-desktop > div > a')].forEach(item => {
+        item.classList.remove('border-b-2', 'border-lime')
+     })
+
+    
+    
+    // .classList.add('border-b-2', 'border-lime')
+}
